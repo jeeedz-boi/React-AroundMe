f7f524cb75492ec0b02a30d05e0dcee2766ca36c
Thongchai Yaemsuk <thongchai@bluebik.com>
2022-03-21 16:20:09 +0700

    [add] image asset and its index file

---
+++ src/app/shared/assets/images/index.js
@@ -0,0 +1,35 @@
+import mainIcon from "./main-logo.png"
+import backIcon from "./arrows/left-arrow.png"
+import downIcon from "./arrows/down-arrow.png"
+import openIcon from "./details/open-icon.png"
+import closeIcon from "./details/close-icon.png"
+import infoIcon from "./details/info-icon.png"
+import ratingIcon from "./details/rating-icon.png"
+import gMapsIcon from "./details/gmaps-icon.png"
+import barIcon from "./location-types/bar-icon.png"
+import foodIcon from "./location-types/food-icon.png"
+import healthIcon from "./location-types/health-icon.png"
+import hospitalIcon from "./location-types/hospital-icon.png"
+import poiIcon from "./location-types/poi-icon.png"
+import restaurantIcon from "./location-types/restaurant-icon.png"
+import shoppingIcon from "./location-types/shopping-icon.png"
+import storeIcon from "./location-types/store-icon.png"
+
+export { 
+    mainIcon,
+    backIcon,
+    downIcon,
+    openIcon,
+    closeIcon,
+    infoIcon,
+    ratingIcon,
+    gMapsIcon,
+    barIcon,
+    foodIcon,
+    healthIcon,
+    hospitalIcon,
+    poiIcon,
+    restaurantIcon,
+    shoppingIcon,
+    storeIcon,
+}
